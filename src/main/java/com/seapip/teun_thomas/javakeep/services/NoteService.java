50c1f8f2cca127825e09f3f68b97c252f6713b32
seahorsepip <seahorsepip@outlook.com>
2017-11-14 14:09:55 +0100

    Refactor

--- src/main/java/com/seapip/teun_thomas/javakeep/services/NoteService.java
+++ src/main/java/com/seapip/teun_thomas/javakeep/services/NoteService.java
@@ -1,9 +1,7 @@
 package com.seapip.teun_thomas.javakeep.services;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 @Path("/notes")
