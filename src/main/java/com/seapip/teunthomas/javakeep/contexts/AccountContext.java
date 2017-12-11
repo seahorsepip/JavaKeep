e4cb100a81ab53e653ba252455d1daf1206f60aa
joeyboy108 <teun.de.wijs@live.nl>
2017-12-11 22:18:26 +0100

    updated

--- src/main/java/com/seapip/teunthomas/javakeep/contexts/AccountContext.java
+++ src/main/java/com/seapip/teunthomas/javakeep/contexts/AccountContext.java
@@ -3,5 +3,9 @@ package com.seapip.teunthomas.javakeep.contexts;
 import com.seapip.teunthomas.javakeep.entities.Noteable;
 
 public interface AccountContext {
-    Accountable getById(Long id, Long accountId);
+    Accountable getById(Long id);
+    Accountable getByEmail(String email);
+    void create(String email, String password);
+    void delete(Long id);
+    void update(String newEmail, String newPassword);
 }
